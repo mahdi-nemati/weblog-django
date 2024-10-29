70f3f1bd2e5a9e0f175169653184894e1cace510
Mahdi Nemati <this.nemati@gmail.com>
2024-10-29 17:30:07 +0330

    add posts to installed app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
+    author = models.CharField(max_length=20)
+    created_at = models.DateField()
